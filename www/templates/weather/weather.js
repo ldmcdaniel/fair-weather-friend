7d170405b03fb5203f45bc001a7da21360e35bd3
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-07-14 15:17:23 -0500

    receiving weather for selected city

--- www/templates/weather/weather.js
+++ www/templates/weather/weather.js
@@ -1,5 +1,13 @@
 angular.module('fwf.weather', [])
 
-  .controller('WeatherCtrl', function () {
+  .controller('WeatherCtrl', function ($scope, $stateParams, $http) {
+
+    $scope.city = $stateParams.city;
+
+    $http
+      .get('/api/forecast/' + $stateParams.lat + ',' + $stateParams.long)
+      .success(function (data) {
+        console.log(data);
+      })
 
   });
