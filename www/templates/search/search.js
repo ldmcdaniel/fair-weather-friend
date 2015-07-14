95d6e7090fffab08a6fbfd152406df6c7883f3c9
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-07-14 12:30:22 -0500

    returning search objects

--- www/templates/search/search.js
+++ www/templates/search/search.js
@@ -1,4 +1,15 @@
 angular.module('fwf.search', [])
-.controller('SearchCtrl', function () {
 
-})
+  .controller('SearchCtrl', function ($scope, $http) {
+    //Debounce will regulate when information is sent from query
+
+    $scope.queryChanged = _.debounce(function () {
+      $http
+        .get('https://maps.googleapis.com/maps/api/geocode/json', {
+          params: { address: $scope.query }
+        })
+        .success(function (data) {
+          console.log(data);
+        });
+    }, 2000);
+  });
